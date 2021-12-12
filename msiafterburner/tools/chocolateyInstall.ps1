89c052ef659a9d8113cc9b6923a6acc98eea5c16
HunterZ <108939+HunterZ@users.noreply.github.com>
2021-12-11 17:15:33 -0800

    msiafterburner: update checksum

--- msiafterburner/tools/chocolateyInstall.ps1
+++ msiafterburner/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'msiafterburner'
 $url = 'http://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=' + $(Invoke-RestMethod https://www.msi.com/api/v1/get_token?date=$(Get-Date -format "yyyyMMdd"))
-$checksum = '521CDBE532A8A8DFB3217A4B0B99C6B8F2709B90DCF0D764DBBCBF4B8F1230F9'
+$checksum = '42B257623C9445D5BC5EEDDD44DA8CC885C43A16FD2A98077338F937B777EAA3'
 $checksumtype = 'sha256'
 $unpackDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $unpackFile = Join-Path $unpackDir 'afterburner.zip'
@@ -13,13 +13,14 @@ $file = (Get-ChildItem -Path $unpackDir -Recurse | Where-Object { $_.Name -match
 
 $waitseconds = 2
 foreach ($procstring in @("MSIAfterburner*", "RTSS", "EncoderServer*", "RTSSHooksLoader*")) {
-  Write-Output "Checking whether process matching $procstring is running..."
+#  Write-Output "Checking whether process matching $procstring is running..."
   for ($i = 1; $i -le 2; $i++) {
     $procobj = Get-Process $procstring -ErrorAction SilentlyContinue
     if ($null -eq $procobj) {
       # Write-Output " SUCCESS - No process matching $procstring is running"
       break
     }
+    Write-Output "Attempting to stop process $procstring..."
     Stop-Process -Name $procstring
     # Write-Output " Try # $i : Waiting $waitseconds seconds for process to terminate..."
     Start-Sleep -s $waitseconds
