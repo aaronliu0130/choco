675d4005cd14efbd86c777fe6a878bac032fcf05
Aaron Liu <aaronliu0130@gmail.com>
2022-11-19 11:39:01 -0500

    4.6.5

--- msiafterburner/tools/chocolateyInstall.ps1
+++ msiafterburner/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'msiafterburner'
 $url = 'http://download.msi.com/uti_exe/vga/MSIAfterburnerSetup.zip?__token__=' + $(Invoke-RestMethod https://www.msi.com/api/v1/get_token?date=$(Get-Date -format "yyyyMMdd"))
-$checksum = '42B257623C9445D5BC5EEDDD44DA8CC885C43A16FD2A98077338F937B777EAA3'
+$checksum = '43E5CFAC9AA9560B6294CEC26578A26F83AA597E9AB8419DA47FB90E6ECEA202'
 $checksumtype = 'sha256'
 $unpackDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $unpackFile = Join-Path $unpackDir 'afterburner.zip'
